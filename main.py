e0a0134e017faf1d37397eb077e7030026821a62
mauriziopolizzi <mauriziopolizzi@mauriziopolizzis-MacBook-Pro.local>
2025-08-01 09:49:17 +0200

    json output format

--- main.py
+++ main.py
@@ -3,20 +3,27 @@ from langchain_ollama.llms import OllamaLLM
 from langgraph.graph import START, END, StateGraph
 from typing import Annotated, TypedDict, Union
 from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
 from prompt import prompt
+import json
 
-# Initialize Ollama client
 model = "mistral:latest"
 llm = OllamaLLM(model=model)
 
+
 class State(TypedDict):
     messages : list[str]
+    agent_choice : Annotated[list[str],add_messages]
 
+config = {"configurable": {"thread_id": "1"}}
 graph_builder = StateGraph(State)
 
 
 def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+    messages = llm.invoke(state["messages"])
+    if isinstance(messages, str):
+        response= json.loads(messages)
+    return {"messages": messages, "agent_choice": response["agent_choice"]}
 
 graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_edge(START, "chatbot")
@@ -28,7 +35,7 @@ graph = graph_builder.compile()
 def stream_graph_updates(user_input: str):
     for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
         for value in event.values():
-            print("Assistant:", value["messages"][-1])
+            print("Assistant:", value["agent_choice"])
 
 
 while True:
--- prompt.py
+++ prompt.py
@@ -5,15 +5,18 @@ Game rules:
 - You and another agent have been arrested for a crime. You cannot communicate with each other.
 - Each of you must independently choose one of two actions: COOPERATE (stay silent) or DEFECT (betray the other).
 - The outcomes are as follows:
-    - If you both COOPERATE: both receive a moderate sentence.
-    - If you DEFECT and the other COOPERATES: you go free, the other receives a heavy sentence.
-    - If you COOPERATE and the other DEFECTS: you receive a heavy sentence, the other goes free.
-    - If you both DEFECT: both receive a harsh sentence.
+    - If you both COOPERATE: both receive a moderate sentence (1)
+    - If you DEFECT and the other COOPERATES: you go free (0), the other receives a heavy sentence (2).
+    - If you COOPERATE and the other DEFECTS: you receive a heavy sentence (2), the other goes free (0).
+    - If you both DEFECT: both receive a harsh sentence (3).
 
 Your task:
 - Decide whether to COOPERATE or DEFECT.
-- Justify your choice in 1-2 sentences.
 - Remember, this is a one-time decision (no future rounds).
 
-Respond with your choice ("COOPERATE" or "DEFECT") and your reasoning.
+***IMPORTANT***
+- the output must be in json format:
+{
+    "agent_choice": "COOPERATE" or "DEFECT" ,
+}
 """
